c9335f20122c24185c05dd8d2d65391232eb53e4
T.U <trinhuyenlht7@gmail.com>
2024-10-04 13:20:31 +0700

    39

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,9 +1,11 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  const { todos } = props;
   return (
     <div className="todo-data">
       <div>Play bida</div>
       <div>Play Badminton</div>
       <div>My Name is {name}</div>
+      <div>{JSON.stringify(props.todos)}</div>
     </div>
   );
 };
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 const TodoNew = (props) => {
+  const [todoName, setTodoName] = useState("");
+
   const { addNewTodo } = props;
+  const handleOnChangeTodoInput = (todoName) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleOnClickAdd = () => {
+    console.log(todoName);
+  };
+
   return (
     <div className="todo-new">
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(event) => {
+          handleOnChangeTodoInput(event.target.value);
+        }}
+      />
       <button
         onClick={() => {
-          addNewTodo("Chin KaiDo");
+          handleOnClickAdd();
         }}
       >
         ADD
       </button>
+      <div>new todo : {todoName}</div>
     </div>
   );
 };
