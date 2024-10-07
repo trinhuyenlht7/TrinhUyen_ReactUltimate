bdaa04b7a80d7adcc9ec7c96664ea735e7834290
T.U <trinhuyenlht7@gmail.com>
2024-10-07 14:28:45 +0700

    add Axios call api

--- src/components/user/UserForm.jsx
+++ src/components/user/UserForm.jsx
@@ -1,27 +1,63 @@
 import { Input } from "antd";
 import "./UserForm.css";
+import { useState } from "react";
 
 const UserForm = () => {
+  const [userName, setUserName] = useState([]);
+  const [userPhone, setUserPhone] = useState([]);
+  const [userEmail, setUserEmail] = useState([]);
+  const [userPass, setUserPass] = useState([]);
+
+  const handleOnClickAddUser = () => {
+    console.log({ userName, userPass, userPhone, userEmail });
+  };
+
   return (
     <div className="user-form">
       <div>
         <span>Full Name</span>
-        <Input />
+        <Input
+          value={userName}
+          onChange={(event) => {
+            setUserName(event.target.value);
+          }}
+        />
       </div>
       <div>
         <span>Phone Number</span>
-        <Input />
+        <Input
+          value={userPhone}
+          onChange={(event) => {
+            setUserPhone(event.target.value);
+          }}
+        />
       </div>
       <div>
         <span>Email</span>
-        <Input />
+        <Input
+          value={userEmail}
+          onChange={(event) => {
+            setUserEmail(event.target.value);
+          }}
+        />
       </div>
       <div>
         <span>Password</span>
-        <Input.Password />
+        <Input.Password
+          value={userPass}
+          onChange={(event) => {
+            setUserPass(event.target.value);
+          }}
+        />
       </div>
       <div className="button-container">
-        <button>Create User</button>
+        <button
+          onClick={() => {
+            handleOnClickAddUser();
+          }}
+        >
+          Add User
+        </button>
       </div>
     </div>
   );
